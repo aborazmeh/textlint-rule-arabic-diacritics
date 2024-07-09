f37d6c7b0bebe5f2c8afe8d87dfdf5119b4a3c2b
Abdurrahman Aborazmeh <aborazmeh@yahoo.com>
2024-07-09 12:39:09 +0300

    move code to noLooseDiacritics function

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,43 @@
-import type { TextlintRuleModule } from "@textlint/types";
+import { TxtStrNode } from "@textlint/ast-node-types";
+import type { TextlintRuleContext, TextlintRuleModule } from "@textlint/types";
 
 export interface Options {
     remove_loose_diacritics?: boolean;
 }
 
+function noLooseDiacritics(
+    node: TxtStrNode,
+    text: string,
+    context: Readonly<TextlintRuleContext>,
+    removeLooseDiacritics: boolean
+) {
+    const { fixer, report, locator, RuleError } = context;
+    const matches = text.matchAll(/\s[\u064B-\u0653]/g);
+    for (const match of matches) {
+        const index = match.index ?? 0;
+        const matchRange = [index, index + match[0].length] as const;
+        const replace = fixer.replaceTextRange([index + 1, index + match[0].length], "");
+        const errObj = removeLooseDiacritics
+            ? {
+                  padding: locator.range(matchRange),
+                  fix: replace
+              }
+            : {
+                  padding: locator.range(matchRange)
+              };
+        const ruleError = new RuleError("Found loose arabic diacritic.", errObj);
+        report(node, ruleError);
+    }
+}
+
 const report: TextlintRuleModule<Options> = (context, options = {}) => {
-    const { Syntax, RuleError, fixer, report, getSource, locator } = context;
+    const { getSource, Syntax } = context;
     return {
         [Syntax.Str](node) {
             const removeLooseDiacritics = options.remove_loose_diacritics ?? true;
+
             const text = getSource(node); // Get text
-            const matches = text.matchAll(/\s[\u064B-\u0653]/g);
-            for (const match of matches) {
-                const index = match.index ?? 0;
-                const matchRange = [index, index + match[0].length] as const;
-                const replace = fixer.replaceTextRange([index + 1, index + match[0].length], "");
-                const errObj = removeLooseDiacritics
-                    ? {
-                          padding: locator.range(matchRange),
-                          fix: replace
-                      }
-                    : {
-                          padding: locator.range(matchRange)
-                      };
-                const ruleError = new RuleError("Found loose arabic diacritic.", errObj);
-                report(node, ruleError);
-            }
+            noLooseDiacritics(node, text, context, removeLooseDiacritics);
         }
     };
 };
